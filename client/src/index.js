c8900aec5abcb3e7070f6a119d4c3bd97c0cde08
Abhay Sahani <abhay.cs.777@gmail.com>
2024-01-19 11:59:46 +0530

    Created Server & Client and conected the Mongodb

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import thunk from 'redux-thunk';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
