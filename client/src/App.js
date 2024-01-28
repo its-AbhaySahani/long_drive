34f4e4e978c160c34f886e8a11c54c0cb4816c17
Abhay Sahani <abhay.cs.777@gmail.com>
2024-01-29 01:49:52 +0530

    Use Mui and made signin page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import SignInSide from "./components/Signin";
 //import { useState } from "react/cjs/hooks";
+
 
 const App = () => {
     return (
             <div>
-                <h1>App</h1>
+                <SignInSide/>
             </div>
     );
 }
