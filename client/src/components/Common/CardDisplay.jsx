36c751f6aabce3222e395038292ffb1b4e32d0bf
Abhay Sahani <abhay.cs.777@gmail.com>
2024-02-05 16:13:53 +0530

    For netlify

---
+++ client/src/components/Common/CardDisplay.jsx
@@ -0,0 +1,14 @@
+import ActionAreaCard from './CommonC.jsx';
+import './style.css';
+function CardDisplay({headline, cards}) {
+    return (
+        <div className="garage">
+            <h1 className="heading">{headline}</h1>
+            <div className="car-cards">
+                { cards.map((c, index)=><ActionAreaCard key={index}/>) }
+            </div>
+        </div>
+    );
+}
+
+export default CardDisplay;
