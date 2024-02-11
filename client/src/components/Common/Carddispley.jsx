d0ea4749a555509e2b6844a8530d96cd8187b807
Abhay Sahani <abhay.cs.777@gmail.com>
2024-02-12 04:10:20 +0530

    updated ui

--- client/src/components/Common/Carddispley.jsx
+++ client/src/components/Common/Carddispley.jsx
@@ -6,7 +6,7 @@ import './style.css';
 function CardDisplay({ headline, cards }) {
 
     return (
-        <div style={{display:"block"}}>
+        <div style={{margin: "0 20px"}}>
             <div className="garage">
                 <h1 className="heading">{headline}</h1>
                 <div className="car-cards">
